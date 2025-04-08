ab2c316120e8b659994397a5688e3991fcef417f
YamanoYuma <mrp.yamano@gmail.com>
2025-04-09 08:37:58 +0900

    19レビュー後

--- 19/index.js
+++ 19/index.js
@@ -5,9 +5,12 @@ const intervalId = setInterval(() => {
   if (count % 3 === 0) {
     console.log("世界のナベアツ");
   } else if (count.toString().includes('3')) {
-    console.log(`世界のナベアツ${count}回目`);
+    console.log(`世界のナベアツ：${count}回目`);
   } else {
     console.log(`処理回数：${count}回目`);
+  }
+  if (count === MAX_COUNT){
+    clearInterval(intervalId);
   }
   count++;
 }, 1000);
