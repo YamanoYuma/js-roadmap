cb614388103ba5e5d1bb3513911f6bf0cafb3773
YamanoYuma <mrp.yamano@gmail.com>
2025-04-09 22:27:03 +0900

    一旦クリア

--- 21/index.js
+++ 21/index.js
@@ -1,5 +1,7 @@
 const greetingElement = document.getElementById('sayHello');
 
+// TODO: 現在の時刻を取得して昼ならこんにちは、夜ならこんばんはになるようにする関数を追加
+
 function sayHello(name) {
   const greetingMessage = `こんにちは！${name}さん！`;
   greetingElement.textContent = greetingMessage;
--- 22/index.js
+++ 22/index.js
@@ -1,7 +1,10 @@
 const sumElement = document.getElementById('sum');
 
-function sum (a , b) {
-  sumElement.textContent = a + b;
+function sum (num1, num2) {
+  // TODO: 引数が数値かどうかのバリデーションを追加する
+  const result = num1 + num2;
+  sumElement.textContent = result;
+  return result;
 }
 
 sum(100,100);
