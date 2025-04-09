3f524e37d5cde59fcdc7d96cfe5b933b53428716
YamanoYuma <mrp.yamano@gmail.com>
2025-04-09 21:52:13 +0900

    20レビュー前

--- 20/index.js
+++ 20/index.js
@@ -1,24 +1,36 @@
+const timeText = document.getElementById('time');
+const dateText = document.getElementById('date');
+const nextFiscalYearText = document.getElementById('nextFiscalYear');
+
 const d = new Date();
 
-console.log(d.toString());
+// 現在時刻:hh時mm分ss秒
+const hour = d.getHours();
+const minutes = d.getMinutes();
+const seconds = d.getSeconds();
 
+const hhmmss = `${hour}時${minutes}分${seconds}秒`;
+
+timeText.textContent = hhmmss;
+
+// 今日の日付(yyyy年mm月dd日)
+// 応用として曜日も表示
 const year = d.getFullYear();
 const month = String(d.getMonth() + 1).padStart(2, '0');
 const date = String(d.getDate() + 1).padStart(2, '0');
 const dayofweek = d.getDay();
 
-
 const dayName = ['日','月','火','水','木','金','土'];
 
 const yyyymmdd = `${year}年${month}月${date}日(${dayName[dayofweek]})`
 
-console.log(yyyymmdd);
+dateText.textContent = yyyymmdd;
 
+// 来年の年度(yyyy年)
 // 今年度
 const fiscalYear = month >= 4 ? year : year -1;
 
-console.log(`今年度は${fiscalYear}年度`);
-
 // 来年度
-const nextFiscalYear = year + 1;
-console.log(`来年度は${nextFiscalYear}年度`);
+const nextFiscalYear = `${fiscalYear + 1}年`;
+
+nextFiscalYearText.textContent = nextFiscalYear;
