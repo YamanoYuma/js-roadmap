7f9527d9cd9f781ca828c10e87caeee1d9d6b487
YamanoYuma <mrp.yamano@gmail.com>
2025-04-10 08:58:25 +0900

    時刻によって挨拶の種類を出し分ける処理を追加

--- 21/index.js
+++ 21/index.js
@@ -1,10 +1,22 @@
-const greetingElement = document.getElementById('sayHello');
+const greetingElement = document.getElementById('greeting');
 
 // TODO: 現在の時刻を取得して昼ならこんにちは、夜ならこんばんはになるようにする関数を追加
+function getGreetingByTime() {
+  const d = new Date();
+  const hour = d.getHours();
+  if (hour >= 6 && hour < 12) {
+    return "おはようございます！";
+  } else if (hour >= 12 && hour < 18) {
+    return "こんちにちは！";
+  } else {
+    return "こんばんは！";
+  }
+}
 
-function sayHello(name) {
-  const greetingMessage = `こんにちは！${name}さん！`;
+function dispayGreeting(name) {
+  const greeting = getGreetingByTime();
+  const greetingMessage = `${greeting}${name}さん！`;
   greetingElement.textContent = greetingMessage;
 }
 
-sayHello('takeっち');
+dispayGreeting('takeっち');
